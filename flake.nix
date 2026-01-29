378a0f930dade82ab81e3299e7a25e4d38e4112e
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-01-29 19:28:16 +1100

    INit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.corepack
+
+            pkgs.pnpm
+
+            pkgs.python3
+          ];
+        };
+      });
+    };
+}
