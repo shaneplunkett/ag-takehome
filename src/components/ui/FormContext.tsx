3a0318ed312701b939cd9cb73d5ca91feeef4fee
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-02-18 21:42:03 +1100

    Context

--- src/components/ui/FormContext.tsx
+++ src/components/ui/FormContext.tsx
@@ -1,3 +1,54 @@
+import { createContext } from "react";
+import { useState } from "react";
+import type { PropsWithChildren } from "react";
+
+interface VehicleContext {
+  make: string;
+  model: string;
+  badge: string;
+  handleMakeChange: (make: string) => void;
+  handleModelChange: (model: string) => void;
+  handleBadgeChange: (badge: string) => void;
+}
+
+const FacetContext = createContext<VehicleContext | null>(null);
+
+export function VehicleProvider({ children }: PropsWithChildren) {
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [badge, setBadge] = useState("");
+
+  const handleMakeChange = (value: string) => {
+    setModel("");
+    setBadge("");
+    setMake(value);
+  };
+
+  const handleModelChange = (value: string) => {
+    setBadge("");
+    setModel(value);
+  };
+
+  const handleBadgeChange = (value: string) => {
+    setBadge(value);
+  };
+
+  return (
+    <FacetContext.Provider
+      value={{
+        make,
+        model,
+        badge,
+        handleMakeChange,
+        handleModelChange,
+        handleBadgeChange,
+      }}
+    >
+      {children}
+    </FacetContext.Provider>
+  );
+}
+
 export default function FormContext() {
   return <div>Sup</div>;
 }
